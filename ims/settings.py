9036d9c5bb515018a8ad2dd8386b602c67ed9b67
suyog khadka <ksuyog697@gmail.com>
2026-08-22 22:20:57 +0545

    added config

--- ims/settings.py
+++ ims/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&q+%$c09!ya2fhj#5ry8abnl09i(l0-=)y)5k*q1e10lleau06"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False )
 
 ALLOWED_HOSTS = []
 
