b1c145d224d73d7ad9b38e79ed2722568c7a37d4
suyog khadka <ksuyog697@gmail.com>
2026-09-22 16:21:23 +0545

    chore: add default DRF pagination settings

--- ims/settings.py
+++ ims/settings.py
@@ -167,6 +167,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=False, cast=bool)
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
+
 
 CKEDITOR_5_CONFIGS = {
     "default": {
